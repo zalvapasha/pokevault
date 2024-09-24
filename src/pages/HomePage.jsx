6db6affb2f499a63cc8b5e10e297c362f39d246d
itsbyferdi <work.hafidhferdi@gmail.com>
2024-09-24 12:04:17 +0700

    Configure pokemon types tag

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Types from "../components/Types";
 
 const HomePage = () => {
-  return <div className="text-3xl font-bold underline">HomePage</div>;
+  return (
+    <>
+      <div className="text-3xl font-bold underline">HomePage</div>
+      <div className="flex flex-col gap-1 w-max">
+        {" "}
+        <Types type="normal" />
+        <Types type="fire" />
+        <Types type="water" />
+        <Types type="electric" />
+        <Types type="grass" />
+        <Types type="ice" />
+        <Types type="fighting" />
+        <Types type="poison" />
+        <Types type="flying" />
+        <Types type="psychic" />
+        <Types type="ground" />
+        <Types type="bug" />
+        <Types type="rock" />
+        <Types type="ghost" />
+        <Types type="dragon" />
+        <Types type="dark" />
+        <Types type="steel" />
+        <Types type="fairy" />{" "}
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
