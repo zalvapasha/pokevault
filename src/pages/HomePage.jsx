5412e27f7df65e5d3cf1e90e15d1c5a07eefd272
zalvapasha <zalvapasha1@gmail.com>
2024-09-24 15:51:58 +0700

    feat: add card component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Card from "../components/Card";
 
 const HomePage = () => {
-  return <div className="text-3xl font-bold underline">HomePage</div>;
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">HomePage</div>
+      <Card />
+    </div>
+  );
 };
 
 export default HomePage;
