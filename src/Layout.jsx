db015fe653fb7287483f7d443eeef1e90f28c740
zalvapasha <zalvapasha1@gmail.com>
2024-09-27 02:31:01 +0700

    feat: react-icons, navbar, and little bit styling

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 const Layout = () => {
   return (
     <main>
+      <NavBar />
       <Outlet />
     </main>
   );
